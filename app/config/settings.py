3ee068b2df14f1f9a8fa7d055207c9bb21c43679
SeungYeob Baek <superior051@icloud.com>
2021-09-29 10:20:03 +0900

    docs: Change settings.py (allow_host, tz)
    - ALLOW_HOST
    - TIME_ZONE
    - LANGUAGE_CODE
    - INSTALLED_APPS

--- app/config/settings.py
+++ app/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = env.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -106,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
